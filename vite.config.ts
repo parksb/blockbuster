0cc244ccb29dd0d98ab55a2b49356b83d84a362a
parksb <parkgds@gmail.com>
2023-11-18 21:48:03 +0900

    파이어베이스 배포를 위해 재설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+//@ts-ignore
+import path from 'path';
 import { defineConfig } from 'vite';
 
+import { sveltekit } from '@sveltejs/kit/vite';
+
+//@ts-ignore
+
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+
+  resolve: {
+    alias: {
+      //@ts-ignore
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
