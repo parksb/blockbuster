d2cef8103de6632fe808b3e1506253bf4f7957aa
parksb <parkgds@gmail.com>
2023-11-18 20:08:42 +0900

    스켈레톤 구성

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="root">
+  <div class="side"></div>
+  <div class="main">
+    <div class="top">
+      <h2>Overview</h2>
+    </div>
+    <div class="content">
+      <div class="card">A</div>
+      <div class="card">B</div>
+      <div class="card">C</div>
+      <div class="card">D</div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .root {
+    font-family: sans-serif;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    overflow: hidden;
+  }
+
+  .side {
+    background-color: #fff;
+    width: 280px;
+    height: 100%;
+    border-right: 1.5px solid #e1e6eb;
+  }
+
+  .main {
+    background-color: #fafbfc;
+    width: 100%;
+    height: 100%;
+    padding: 10px 40px;
+    overflow: auto;
+
+    .top {
+      & > h2 {
+        color: #1e2860;
+      }
+    }
+
+    .content {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 25px;
+
+      .card {
+        background-color: #fff;
+        border: 1.5px solid #e1e6eb;
+        border-radius: 5px;
+        padding: 15px;
+      }
+    }
+  }
+</style>
