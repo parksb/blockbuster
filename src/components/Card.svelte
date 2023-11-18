27576bb700fad28b19b51a871cdca64b7912636f
parksb <parkgds@gmail.com>
2023-11-18 20:49:47 +0900

    카드 요소 컴포넌트화

---
+++ src/components/Card.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let title: string;
+</script>
+
+<div class="card">
+  <div class="header">{title}</div>
+  <div class="body"><slot /></div>
+</div>
+
+<style lang="scss">
+  .card {
+    background-color: #fff;
+    border: 1.5px solid #e1e6eb;
+    border-radius: 5px;
+    margin-bottom: var(--marginBottom);
+
+    .header {
+      padding: 15px;
+      border-bottom: 1.5px solid #e1e6eb;
+      font-weight: 700;
+      color: var(--color-main);
+    }
+
+    .body {
+      padding: 15px;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
+<script lang="ts">
+  import Card from "../components/Card.svelte";
+</script>
+
+<svelte:head>
+	<title>Blockbuster</title>
+</svelte:head>
+
 <div class="root">
   <div class="side"></div>
   <div class="main">
-    <div class="top">
+    <div class="header">
       <h2>Overview</h2>
     </div>
-    <div class="content">
-      <div class="card">A</div>
-      <div class="card">B</div>
-      <div class="card">C</div>
-      <div class="card">D</div>
+    <div class="body">
+      <Card title="Title A" --marginBottom="30px" />
+      <Card title="Title B" />
     </div>
   </div>
 </div>
@@ -23,36 +29,32 @@
   }
 
   .side {
-    background-color: #fff;
+    background-color: var(--color-white);
     width: 280px;
     height: 100%;
-    border-right: 1.5px solid #e1e6eb;
+    border-right: 1.5px solid var(--color-line);
   }
 
   .main {
-    background-color: #fafbfc;
+    background-color: var(--color-bg);
     width: 100%;
     height: 100%;
     padding: 10px 40px;
     overflow: auto;
 
-    .top {
+    .header {
       & > h2 {
-        color: #1e2860;
+        color: var(--color-main);
       }
     }
+  }
 
-    .content {
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr;
-      gap: 25px;
-
-      .card {
-        background-color: #fff;
-        border: 1.5px solid #e1e6eb;
-        border-radius: 5px;
-        padding: 15px;
-      }
-    }
+  :global(:root) {
+    --color-emphasis: #1325AE;
+    --color-main: #1e2860;
+    --color-description: #8f94b0;
+    --color-line: #e1e6eb;
+    --color-bg: #fafbfc;
+    --color-white: #ffffff;
   }
 </style>
